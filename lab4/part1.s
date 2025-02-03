cb6f41c6e8b9c3ff00e584706168d2ebfb9855a5
Ryan Neil Alumkal <alumkalryan@gmail.com>
2025-02-03 15:06:06 -0500

    Update part1.s

--- lab4/part1.s
+++ lab4/part1.s
@@ -1,78 +1,52 @@
 .global _start
 _start:
-	
-LED:
-	li a0, 0xFF200000 #LED PIT/OUTPUT
-	li a1, 1 #stores numbers for LED
-	sw x0, (a0)
-BUTTON:
-	li t0, 0xFF200050 #BUTTON PIT/OUTPUT
+    li t0, 0xFF200000  
+    li t1, 0xFF200050  
+    li s1, 0          
+	li t5, 1
 
-BUTTON_PRESSED_LOOP:
-	lw t1, (t0) #read BUTTON PIT DR
-	lw t6, (t0) #stores the original t1 val
-	andi t2, t1, 1
-	bne t2, x0, BUTTON_RELEASED_LOOP # checks if button 0 is pressed
-	
-	srli t1, t1, 1 #shift bit
-	andi t2, t1, 1
-	bne t2, x0, BUTTON_RELEASED_LOOP # checks if button 1 is pressed
-	
-	srli t1, t1, 1 #shift bit
-	andi t2, t1, 1
-	bne t2, x0, BUTTON_RELEASED_LOOP # checks if button 2 is pressed
-	
-	srli t1, t1, 1 #shift bit
-	andi t2, t1, 1
-	bne t2, x0, BUTTON_RELEASED_LOOP # checks if button 3 is pressed
-	
-	j BUTTON_PRESSED_LOOP
+pressed:
+    lw a0, 0(t1)      
+    beq a0, x0, pressed  # waiting till pressed
+mv t4, a0          # Store pressed button value
 
-BUTTON_RELEASED_LOOP:
-	lw t3, (t0) #read BUTTON PIT DR
-	#CHECK IF KEY3 WAS PREVIOUSLY PRESSED, IF IT WAS AND BUTTONS ARE PRESSED
-	# DO SPECIAL FUNC
-	beq t6, t3, BUTTON_RELEASED_LOOP #IF BUTTON HAS NOT BEEN UPDATED
+releasecheck:
+    lw a0, 0(t1)      
+    bne a0, x0,releasecheck  # waiting till released
+	beq t6, t5, SETTOONE   
+    andi t3, t4, 1     
+    bne t3, x0, SETTOONE
+	andi t3, t4, 2   
+    bne t3, x0, INCREMENT
+	andi t3, t4, 4    
+    bne t3, x0, DECREMENT
 
-CHECK_COND:
-	andi t4, t6, 1 #check 0 BUTTON
-	bne t4, x0, DISPLAY # checks if button 0 is released
-	
-	srli t6, t6, 1 #shift by 1 bit
-	andi t4, t6, 1
-	bne t4, x0, INCREMENT #checks if button 1 is released
-	
-	srli t6, t6, 1 #shift by 1 bit
-	andi t4, t6, 1
-	bne t4, x0, DECREMENT #checks if button 1 is released
-	
-	
-	j BUTTON_PRESSED_LOOP
+    andi t3, t4, 8     
+    bne t3, x0, BLANK
 
-DISPLAY: #KEY0 is pressed
-	sw a1, 0(a0) #display 
+    j pressed  
 
-	j BUTTON_PRESSED_LOOP
+DISPLAY:
+    sw s1, 0(t0)  
+    j pressed  
 
-INCREMENT: #KEY1 is pressed
+SETTOONE:
+    li s1, 1
+	li t6, 0
+    j DISPLAY
 
-	li s10, 15
-	beq a1, s10, DISPLAY
-	addi a1, a1, 1
-	j DISPLAY
+INCREMENT:
+    li t2, 15         # Max value allowed
+    bge s1, t2, DISPLAY  
+    addi s1, s1, 1   
+    j DISPLAY
+DECREMENT:
+    li t2, 1         # Min value allowed
+    ble s1, t2, DISPLAY  
+    addi s1, s1, -1  
+    j DISPLAY
 
-DECREMENT: #KEY2 is pressed
-	li s10, 1
-	beq a1, s10, DISPLAY #if button is already 1
-	addi a1, a1, -1
-	j DISPLAY
-
-BLANK_DISPLAY: #KEY3 is pressed 
-
-#TODO
-	
-	li a1, 0
-	sw a1, 0(a0)
-	
-	j BUTTON_PRESSED_LOOP
-	
+BLANK:
+    li s1, 0 
+	li t6, 1
+    j DISPLAY
